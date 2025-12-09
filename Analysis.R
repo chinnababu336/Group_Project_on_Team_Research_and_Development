b9ea0593c6384aecd7ce127ec61bd881c17695cc
Aditya krishna <za25adx@herts.ac.uk>
2025-12-10 03:42:54 +0530

    Rename Cumulative column in food_data

--- Analysis.R
+++ Analysis.R
@@ -5,3 +5,11 @@ food_data <- read_csv("Food_Costs_2013.csv")
 names(food_data)
 
 colnames(food_data)[1] <- "State_Agency"
+
+food_data <- food_data %>%
+  rename(
+    Cumulative_Cost = matches("Cumulative", ignore.case = TRUE)
+  )
+
+
+names(food_data)
