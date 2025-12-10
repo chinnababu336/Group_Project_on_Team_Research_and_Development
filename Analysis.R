719c9198d2722be407fd4dbdda284cf0b2c7f5bf
Chinnalucifer007 <chinnalucifer007@gmail.com>
2025-12-10 12:38:11 +0000

    adding comments on changing the columns names.

---
+++ Analysis.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+# Load your dataset
+food_data <- read_csv("Food_Costs_2013.csv")
+names(food_data)
+
+# renaming the first column to "State_Agency"
+colnames(food_data)[1] <- "State_Agency"
+# Renaming columns that contain the word "Cumulative" to "Cumulative_Cost"
+food_data <- food_data %>%
+  rename(
+    Cumulative_Cost = matches("Cumulative", ignore.case = TRUE)
+  )
+
+
+names(food_data)
